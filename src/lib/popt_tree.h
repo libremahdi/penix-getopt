b9b071e5d26faaf913668799bf08579ca9c9aec9
Mahdi Hosseini Asaad <mahdi.libremann@gmail.com>
2025-09-15 22:43:39 +0330

    The issue with untyped keys was resolved by changing is_alw. ; When keys didn’t have a custom type (were INT or NONE), the input values were not being parsed.

--- src/lib/popt_tree.h
+++ src/lib/popt_tree.h
@@ -6,15 +6,18 @@
 
 #include "pgetopt.h"
 
-static unsigned int is_alw ( pclass *class, char *option )
+static int is_alw ( pclass *class, char *option )
 {
     for ( int classI=0 ; classI < class->alw_size ; ++classI )
     {
+        if ( ( strcmp (class->alw_tree[classI]->name, option) == 0 ) && ( class->alw_tree[classI]->key_type != -1 ) )
+            return -2; // 0 is reserved for flags, and -1 is reserved for errors.
         if ( strcmp (class->alw_tree[classI]->name, option) == 0 )
             return class->alw_tree[classI]->values_size;
     }
     return -1;
 }
+
 
 static unsigned int what_is_ID ( pclass *class, char *option )
 {
