b752315d24c9ee88c039eb8d2c85aa3546bb8c6d
Mahdi Hosseini Asaad <mahdi.libremann@gmail.com>
2025-10-04 17:00:13 +0330

    Added: Exercise 4-keys.c added to examples folder

--- examples/4-keys.c
+++ examples/4-keys.c
@@ -19,14 +19,26 @@ int main ( int argc, char **argv )
     //  { opt_id, opt_name },
         { 1, "long_flag" },
         { 2, "f" },
+        { 3, "long_key" },
+        { 4, "k" },
         EOL
-    };
+    };    
 
     pclass_set_allowed_options ( &class, main_allowed_options ); // This function assigns a PALW to a class.
                                                                  // The function's return value is of type usrerr.
+
+
+    pclass_set_key ( &class, 3, NONE ); // This function designates an option ID as a key.
+    /* In pgetopt, if you don't select an option to be a key, it is considered a Flag. If you configure 
+       it as a key using the pclass_set_key function, it will become a Key.
+     * Masters are a type of option that are not related to keys or flags, and their implementation is completely separate
+    */ 
+
+    pclass_set_key ( &class, 4, NONE );
+
     pinit_parser ( &init, argc, argv ); // As its name suggests, this function performs part of the parsing operation.
     /* Specifically, this function initializes (or populates) the PAVl structure with the values consisting 
-     * of the arguments entered by the user.
+       of the arguments entered by the user.
     */
 
     { // `pclass *class` parsing segment
@@ -35,7 +47,7 @@ int main ( int argc, char **argv )
         while ( ( opt_id = pclass_loop_get_opt_id ( class, i ) ) != -1 ) // this function returns the option ID of the options used by the user
         /* The function pclass_loop_get_opt_id iterates over (or traverses) the PAVl structure.
          * Its first argument is the corresponding class, and the second argument is an index 
-         * that helps with the PAVl traversal
+           that helps with the PAVl traversal
          
          * If there is no data to traverse, the function returns a value of −1.
            ( Option IDs are always a positive number. )
@@ -45,9 +57,29 @@ int main ( int argc, char **argv )
                 case (1): // The number 1 is merely a simple identifier of option! (opt_id).
                     printf ("this is long_flag\n");
                     break;
+
                 case (2):
                     printf ("this is f\n");
                     break;
+
+                case (3):
+                    for ( int vi=0 ; vi < pclass_get_key_size ( class, 3 ) ; ++vi ) // This function returns the count of a key's values.
+                    {
+                        printf ( "long_key = %s\n", pclass_key_loop_get_value ( class, 3, vi ) ); // This function iterates over the values of a key.
+                        /* Its first parameter is the class in which the key opt_id resides.
+                         * The second parameter is the option ID that has been converted into the key (or keys).
+                         * The third parameter is an index for the iteration (or traversal).
+                        */
+                    }
+                    break;
+
+                case (4):
+                    for ( int vi=0 ; vi < pclass_get_key_size ( class, 4 ) ; ++vi )
+                    {
+                        printf ( "k = %s\n", pclass_key_loop_get_value ( class, 4, vi ) );
+                    }
+                    break;
+                
             }
             ++i;
         }
