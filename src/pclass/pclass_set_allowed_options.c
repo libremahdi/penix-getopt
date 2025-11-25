e6b9cde05398347e72ea9622d10fe42d27a0cb48
Mahdi Hosseini Asaad <mahdi.libremann@gmail.com>
2025-11-25 13:47:26 +0330

    Upgrade: pclass.c | Move: (func) pclass_set_allowed_options -> (file) pclass/pclass_set_allowed_options.c
    Moving pclass_set_allowed_options function to a dedicated file

---
+++ src/pclass/pclass_set_allowed_options.c
@@ -0,0 +1,40 @@
+#include "pgetopt.h"
+#include "../lib/popt_class.h"
+#include "../lib/popt_error.h"
+#include "salloc.h"
+
+void pclass_set_allowed_options ( pclass **class, palw *allowed_options )
+
+{   unsigned long int i = 0;
+    unsigned int repetitive_opt_id = 0;
+
+    while (allowed_options[i].option_name != NULL)
+    {
+        if ( ( repetitive_opt_id = is_alw_tree_repetitive_id ( (*class), allowed_options[i].option_id ) ) != -1 )
+        {
+            (*class)->alw_tree[repetitive_opt_id]->names = ( char ** ) realloc ( ( (*class)->alw_tree[repetitive_opt_id]->names ) , ( sizeof (char *) * ( (*class)->alw_tree[repetitive_opt_id]->names_size + 1) ) );
+            (*class)->alw_tree[repetitive_opt_id]->names[(*class)->alw_tree[repetitive_opt_id]->names_size] = allowed_options[i].option_name;
+            ++(*class)->alw_tree[repetitive_opt_id]->names_size;
+            ++i;
+            continue;
+        }
+        (*class)->alw_tree = (struct alw_branch **) realloc ( (*class)->alw_tree, ( sizeof (struct alw_branch *) * ((*class)->alw_size + 1) ) );
+        (*class)->alw_tree[(*class)->alw_size] = salloc ( sizeof (struct alw_branch));
+
+        (*class)->alw_tree[(*class)->alw_size]->names = salloc ( sizeof (char *) );
+        (*class)->alw_tree[(*class)->alw_size]->names[0] = allowed_options[i].option_name;
+        (*class)->alw_tree[(*class)->alw_size]->names_size = 1;
+
+        (*class)->alw_tree[(*class)->alw_size]->opt_id = allowed_options[i].option_id;
+
+        (*class)->alw_tree[(*class)->alw_size]->key_type = VOID;
+
+        (*class)->alw_tree[(*class)->alw_size]->values_size = 0;
+        (*class)->alw_tree[(*class)->alw_size]->values = NULL;
+        ++i;
+        ++(*class)->alw_size;   // also i can write it : (*class)->alw_tree[(*class)->alw_size+i] and remove the ++(*class)->alw_size
+                                // ++(*class)->alw_size from here, and put this line in the out of the while segment :
+                                // (*class)->alw_size += i;
+                                // But it compromises the readability of the code. No?
+    }
+}
