d69043dac3c72cecc46738506ebc94c8a7b90931
Mahdi Hosseini Asaad <mahdi.libremann@gmail.com>
2026-01-28 22:11:42 +0330

    Update: pinit_master.c | Add: attribute((nonnull))

--- src/pinit/pinit_master.c
+++ src/pinit/pinit_master.c
@@ -10,16 +10,8 @@
 #include "master.h"
 #include "pgetopt_alloc.h"
 
-// static int internal_add_hint(struct master_alw *alw_p, palw *allowed_options, unsigned int i ) {
-//     if (allowed_options[i].option_hint) {
-//         alw_p->hints = pgetopt__realloc(alw_p->hints, (sizeof(char *)*(alw_p->hint_size+1)));
-//         alw_p->hints[alw_p->hint_size]=strdup(allowed_options[i].option_hint);
-//         ++alw_p->hint_size;
-//         return 0;
-//     } return 1;
-// }
-
-void pinit_set_allowed_masters (pinit *init, palw *allowed_names) {
+void __attribute__((nonnull))
+pinit_set_allowed_masters (pinit *init, palw *allowed_names) {
     unsigned long int i = 0;
     while (allowed_names[i].option_name != NULL) {
         init->alw_masters = (struct master_alw **) pgetopt__realloc (init->alw_masters, (sizeof (struct master_alw *)*(init->alw_masters_size+1)));
@@ -37,21 +29,25 @@ void pinit_set_allowed_masters (pinit *init, palw *allowed_names) {
     }
 }
 
-int pinit_get_master_argc (pinit  *init) {
+int __attribute__((nonnull))
+pinit_get_master_argc (pinit  *init) {
     return init->avl_master->options_size;
 }
 
-char **pinit_get_master_argv (pinit  *init) {
+char** __attribute__((nonnull))
+pinit_get_master_argv (pinit  *init) {
     return init->avl_master->options;
 }
 
-char *pinit_get_master_name (pinit  *init) {
+char* __attribute__((nonnull))
+pinit_get_master_name (pinit  *init) {
     if (init->avl_master == NULL)
         pgetopt__printerr ("The user has not used any Master, but you want to get the name of the available master!", __LINE__, __FILE__);
     return init->avl_master->name;
 }
 
-int pinit_get_master_id (pinit  *init) {
+int __attribute__((nonnull))
+pinit_get_master_id (pinit  *init) {
     if (init->avl_master == NULL) return -1;
     return init->avl_master->master_id;
 }
